564683a2e4770f54d7690077cc427892ba458946
prathviraj <prathviraj.gavani@servicenow.com>
2018-11-21 21:35:55 +0530

    Register and login user APIs

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-	mongoURI: 'mongodb://raj080:raj080@ds057000.mlab.com:57000/samparka'
+	mongoURI: 'mongodb://raj080:raj080@ds057000.mlab.com:57000/samparka',
+	secretOrKey: 'secret'
 }
