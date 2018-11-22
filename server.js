14baed7830c06058ef37970b1224fa9f40f42676
Prathviraj Gavani <prathviraj080@gmail.com>
2018-11-22 12:18:43 +0530

    Register and login user APIs

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/user');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+
+const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //DB config
 const db = require('./config/keys').mongoURI;
@@ -14,8 +21,12 @@ mongoose
 	.connect(db)
 	.then(() => console.log('MongoDB connected'))
 	.catch( err => console.log(err));
+	
+// Passport middleware
+app.use(passport.initialize());
 
-app.get('/', (req, res) => res.send('Hello World'));
+// Passport Config
+require('./config/passport')(passport);
 
 // Use routes
 app.use('/api/users', users);
