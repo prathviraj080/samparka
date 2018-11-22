130813df1720ecad6dcf49c362d82bb3675cb8a9
prathviraj <prathviraj.gavani@servicenow.com>
2018-11-22 12:00:56 +0530

    Revert "Register and login user APIs"
    This reverts commit 564683a2e4770f54d7690077cc427892ba458946.

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,3 @@
 module.exports = {
-	mongoURI: 'mongodb://raj080:raj080@ds057000.mlab.com:57000/samparka',
-	secretOrKey: 'secret'
+	mongoURI: 'mongodb://raj080:raj080@ds057000.mlab.com:57000/samparka'
 }
--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
+const app = express();
 
 const users = require('./routes/api/user');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
-
-const app = express();
-
-// Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 //DB config
 const db = require('./config/keys').mongoURI;
@@ -21,12 +14,8 @@ mongoose
 	.connect(db)
 	.then(() => console.log('MongoDB connected'))
 	.catch( err => console.log(err));
-	
-// Passport middleware
-app.use(passport.initialize());
 
-// Passport Config
-require('./config/passport')(passport);
+app.get('/', (req, res) => res.send('Hello World'));
 
 // Use routes
 app.use('/api/users', users);
